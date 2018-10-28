ab983e7ac1b452175126d474d3ab164d52ec8b35
smakosh <ismai23l@hotmail.com>
2018-10-28 02:02:47 +0000

    use useEffect

--- src/modules/common/Provider/index.jsx
+++ src/modules/common/Provider/index.jsx
@@ -1,9 +1,18 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Context } from '../Context'
 
 const Provider = ({ children }) => {
     const [theme, setTheme] = useState('light')
     const toggleTheme = value => setTheme(value)
+
+    useEffect(() => {
+        document.title = `${theme} Mode`
+        if (theme === 'light') {
+            document.body.style.background = '#fff'
+        } else {
+            document.body.style.background = '#000'
+        }
+    })
 
     return (
         <Context.Provider value={{
